31ac493685c8b5557e8e188714809a6d52816cbb
abhilashdk2016 <abhilashdk2014@outlook.com>
2025-02-01 20:53:37 +0530

    My Portfolio

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,68 +1,68 @@
 "use client";
 import React from 'react';
 import { motion } from "framer-motion";
-import Link from 'next/link';
-import Image from 'next/image';
-import { FaGithub } from "react-icons/fa";
+//import Link from 'next/link';
+//import Image from 'next/image';
+//import { FaGithub } from "react-icons/fa";
 import jira from "../../assets/jira.png";
 import uptimer from "../../assets/uptimer.png";
 import Card from '@/components/card';
 
-interface ProjectDetails {
-  title: string;
-  summary: string;
-  img?: any;
-  link: string;
-  github?: string;
-}
+// interface ProjectDetails {
+//   title: string;
+//   summary: string;
+//   img?: any;
+//   link: string;
+//   github?: string;
+// }
 
-const FeaturedProject = ({ title, summary, img, link, github}: ProjectDetails) => {
-  return (
-    <article className='w-full flex items-center justify-between rounded-3xl border border-solid border-black bg-white shadow-2xl lg:p-12 relative rounded-br-2xl xs:rounded-2xl xs:rounded-br-3xl p-4 flex-col sm:flex-col lg:flex-row'>
-      <div className='absolute top-0 -right-3 -z-10 w-[100%] h-[103%] rounded-[2.5rem] bg-black  rounded-br-3xl' />
-      {img && <Link href={link} target='_blank'
-        className='w-1/2 cursor-pointer overflow-hidden rounded-lg'
-      >
-        <Image src={img} alt={title} className='w-full h-auto' />
-      </Link>}
-      <div className='w-1/2 flex flex-col items-start justify-between pl-6'>
-        <Link href={link} target='_blank' className='hover:underline underline-offset-2'>
-          <h2 className='text-[#B53389] my-2 w-full text-left text-4xl font-bold'>{title}</h2>
-        </Link>
-        <p className='my-2 font-medium text-black'>{summary}</p>
-        <div className='mt-2 flex items-center'>
-        {github && <Link href={github} target='_blank' className='w-10'>
-          <FaGithub size={30} />
-        </Link>}
-        </div>
-      </div>
-    </article>
-  ) 
-}
+// const FeaturedProject = ({ title, summary, img, link, github}: ProjectDetails) => {
+//   return (
+//     <article className='w-full flex items-center justify-between rounded-3xl border border-solid border-black bg-white shadow-2xl lg:p-12 relative rounded-br-2xl xs:rounded-2xl xs:rounded-br-3xl p-4 flex-col sm:flex-col lg:flex-row'>
+//       <div className='absolute top-0 -right-3 -z-10 w-[100%] h-[103%] rounded-[2.5rem] bg-black  rounded-br-3xl' />
+//       {img && <Link href={link} target='_blank'
+//         className='w-1/2 cursor-pointer overflow-hidden rounded-lg'
+//       >
+//         <Image src={img} alt={title} className='w-full h-auto' />
+//       </Link>}
+//       <div className='w-1/2 flex flex-col items-start justify-between pl-6'>
+//         <Link href={link} target='_blank' className='hover:underline underline-offset-2'>
+//           <h2 className='text-[#B53389] my-2 w-full text-left text-4xl font-bold'>{title}</h2>
+//         </Link>
+//         <p className='my-2 font-medium text-black'>{summary}</p>
+//         <div className='mt-2 flex items-center'>
+//         {github && <Link href={github} target='_blank' className='w-10'>
+//           <FaGithub size={30} />
+//         </Link>}
+//         </div>
+//       </div>
+//     </article>
+//   ) 
+// }
 
-const Project = ({ title, summary, img, link, github}: ProjectDetails) => {
-  return (
-    <article className='w-full flex flex-col items-center justify-center border border-solid border-black bg-white p-6 relative rounded-br-2xl rounded-2xl'>
-      <div className='absolute top-0 -right-3 -z-10 w-[100%] h-[104%] rounded-[2.5rem] bg-black  rounded-br-3xl' />
-      {img && <Link href={link} target='_blank'
-        className='w-full cursor-pointer overflow-hidden rounded-lg'
-      >
-        <Image src={img} alt={title} className='w-full h-auto' />
-      </Link>}
-      <div className='w-full flex flex-col items-start justify-between mt-4'>
-        <Link href={link} target='_blank' className='hover:underline underline-offset-2'>
-          <h2 className='text-[#B53389] my-2 w-full text-left text-3xl font-bold'>{title}</h2>
-        </Link>
-        <p className='my-2 font-medium text-black'>{summary}</p>
-        <div className='mt-2 flex items-center'>
-        {github && <Link href={github} target='_blank' className='w-10'>
-          <FaGithub size={30} />
-        </Link>}
-        </div>
-      </div>
-    </article>
-  ) 
-}
+// const Project = ({ title, summary, img, link, github}: ProjectDetails) => {
+//   return (
+//     <article className='w-full flex flex-col items-center justify-center border border-solid border-black bg-white p-6 relative rounded-br-2xl rounded-2xl'>
+//       <div className='absolute top-0 -right-3 -z-10 w-[100%] h-[104%] rounded-[2.5rem] bg-black  rounded-br-3xl' />
+//       {img && <Link href={link} target='_blank'
+//         className='w-full cursor-pointer overflow-hidden rounded-lg'
+//       >
+//         <Image src={img} alt={title} className='w-full h-auto' />
+//       </Link>}
+//       <div className='w-full flex flex-col items-start justify-between mt-4'>
+//         <Link href={link} target='_blank' className='hover:underline underline-offset-2'>
+//           <h2 className='text-[#B53389] my-2 w-full text-left text-3xl font-bold'>{title}</h2>
+//         </Link>
+//         <p className='my-2 font-medium text-black'>{summary}</p>
+//         <div className='mt-2 flex items-center'>
+//         {github && <Link href={github} target='_blank' className='w-10'>
+//           <FaGithub size={30} />
+//         </Link>}
+//         </div>
+//       </div>
+//     </article>
+//   ) 
+// }
 
 const Portfolio = () => {
   return (
